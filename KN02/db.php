77fc3e3c82bf55eb80c704f86188b68bd7f6168d
Emma Bright <113352515+emmabrght@users.noreply.github.com>
2024-02-26 15:45:40 +0100

    kn02

---
+++ KN02/db.php
@@ -0,0 +1,17 @@
+<?php
+    $servername = "kn02b-db";
+    $username = "dbuser";
+    $password = "dbpassword";
+    $dbname = "mydatabase";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "select Host, User from mysql.user;";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()){
+        echo($row["Host"] . " / " . $row["User"] . "<br />");
+    }
+?>
